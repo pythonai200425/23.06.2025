2e18cbacdcdace5944efa81874d42666279c30e3
pythonai200425 <pythonai200425@gmail.com>
2025-06-26 17:58:05 +0300

    Add files via upload

---
+++ insert_query.sql
@@ -0,0 +1,14 @@
+-- 1
+INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES (1, 'Paul', 32, 'California', 20000.00 )
+
+-- 2
+INSERT INTO COMPANY 
+VALUES (1, 'Paul', 32, 'California', 20000.00 )
+
+-- 3
+INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES 
+(1, 'Paul', 32, 'California', 20000.00 ),
+(1, 'Paul', 32, 'California', 20000.00 ),
+(1, 'Paul', 32, 'California', 20000.00 )
